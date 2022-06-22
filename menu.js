a90851aafccf6fb7828c78db27da528c9c65d7ee
Elhama Tokhi <tokhielhama30@gmail.com>
2022-06-22 06:25:20 -0700

    add javascript linters

--- menu.js
+++ menu.js
@@ -1,14 +1,13 @@
-
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".navMobile");
-const links = document.querySelectorAll(".link");
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.navMobile');
+const links = document.querySelectorAll('.link');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    menu.classList.toggle('open');
-})
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('open');
+});
 
 links.forEach((n) => n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    menu.classList.remove('open');
+  hamburger.classList.remove('active');
+  menu.classList.remove('open');
 }));
