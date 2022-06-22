ba8eb0d6d666bb446ad130da0c774663da7784f4
Elhama Tokhi <tokhielhama30@gmail.com>
2022-06-22 12:33:50 -0700

    add email validation functionality to form

--- script.js
+++ script.js
@@ -179,3 +179,23 @@ closeModalBtn.addEventListener('click', () => {
   modal.style.display = 'none';
   body.style.overflow = 'auto';
 });
+
+const form = document.querySelector('.form');
+const email = document.getElementById('email');
+const error = document.querySelector('small');
+form.addEventListener('submit', (e) => {
+  const message = [];
+
+  if (email.value !== email.value.toLowerCase()) {
+    message.push('Please use lowercase for email address');
+  }
+
+  if (message.length > 0) {
+    e.preventDefault();
+    error.innerHTML = message.join('<br/>');
+    email.style.border = '2px solid #df0000';
+    email.style.borderRadius = '3px';
+    email.style.backgroundColor = 'tomato';
+    error.style.backgroundColor = 'red';
+  }
+});
